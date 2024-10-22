be038c8ef17b4e4a55fe0d80887e56fb5b885a74
itaiS123 <itaishur@gmail.com>
2024-10-22 18:08:48 +0300

    changed most of the logic. Pointed some errors in the code, will work on it later. Added validation functions

--- Project.c
+++ Project.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 #define DIM1 25
-#define DIM2 25
 
 #define ZERO 0
 #define ONE 1
@@ -12,77 +11,88 @@
 #define TRUE !ZERO
 #define FALSE ZERO
 
-// Validation
+// Input validation
 {
+	BOOL Is_row_or_col_good(int row, int col){
+		return !(row >= DIM1 || row <= ZERO || col >= DIM1 || col <= ZERO);
+	}		
+	
+	BOOL Is_input_diagnal(int mat[][],int row, int col){
+		return !(mat[row][col] == mat[row -ONE][col +ONE] || mat[row][col] == mat[row +ONE][col -ONE])
+	}
 	
 	
 	
+	BOOL Is_input_good(int row, int col){
+		return ; // all validate functions
+	}
 }
 
-
-
-
-// Check
+// check
 int Play_Row_Turn() {
     unsigned short row;
     printf("enter row: \n");
     scanf("%d", &row);  
-    // validate if the value entered is good.
-    // if not ask again, else continue    
+	
+	while(Is_row_or_col_good(row)){
+		printf("enter row: \n");
+		scanf("%d", &row);
+	}
     return row;
 }
 int Play_Col_Turn() {
     unsigned short col;
     printf("enter col: \n");
-    scanf("%d", &col);  
-    // validate if the value entered is good.
-    // if not ask again, else continue    
+    scanf("%d", &col);   
+	while(Is_row_or_col_good(col)){
+			printf("enter col: \n");
+			scanf("%d", &col);
+		}	
     return col;
 }
 
-void Kill(int counter, int mat[][], int row, int col) {
-    counter -= mat[row - ONE][col] == ZERO && mat[row + ONE][col] == ZERO && mat[row][col - ONE] == ZERO && mat[row][col + ONE];
-}
-
-BOOL Hit(int mat[][], int row, int col) {
+// check
+void Hit(int mat_opponent[DIM1][DIM1], int mat_guess[DIM1][DIM1], int row, int col) {
     BOOL flag = FALSE;
-    mat[row][col] == ONE ? flag = TRUE : ZERO;
-    mat[row][col] = !flag;
-    return flag;
+    mat_opponent[row][col] == ONE ? flag = TRUE : ZERO;
+    mat_opponent[row][col] = !flag;
+	
+	mat_guess[row][col] = flag;
+	
+	flag ? printf("Hit !") : printf("No hit");
+}
+// kill not working good, need to fix
+void Kill(int counter, int mat[][], int row, int col) {
+    counter -= mat[row -ONE][col] == ZERO && mat[row +ONE][col] == ZERO && mat[row][col -ONE] == ZERO && mat[row][col +ONE];
 }
 
 void Game() {
-	int mat[DIM1][DIM2];
+	int mat_sub_player1[DIM1][DIM1];
+	int mat_guess_player1[DIM1][DIM1];
 	
-	int row, col;
+	int mat_sub_player2[DIM1][DIM1];
+	int mat_guess_player2[DIM1][DIM1];
 	
-    unsigned short players_turn = ONE;
-	unsigned short ships_count_player1 = TEN;
-	unsigned short ships_count_player2 = TEN;
-	
-    BOOL is_hit, is_someone_won = FALSE;
+	unsigned short row, col;    	
+	unsigned short player_turn = ONE;
+	unsigned short subs_count_player1 = TEN;
+	unsigned short subs_count_player2 = TEN;
     
-    while (!is_someone_won) {
-        // do not forget to write what player play!
-        printf("player %d, it's your turn \n", players_turn);
-		
+	// plyers will place their subs here...	
+	// mat_sub_player1 and mat_sub_player2...
+	
+    
+    while (ships_count_player1 > ZERO && ships_count_player2 > ZERO){
+		// player turn
+        printf("player %d, please enter your move \n", player_turn % TWO +ONE);		
 		row = Play_Row_Turn();
-		col = Play_Col_Turn();
+		col = Play_Col_Turn;
 		
-        is_hit = Hit(mat, row(), col());
-		is_hit ? printf("hit !") : printf("no hit");
+		// checking hit and kill
+		player_turn % TWO +ONE == ONE ? Hit(mat_sub_player2, mat_guess_player1, row, col), Kill(subs_count_player2, mat_sub_player2, row, col) : ZERO; 		
+		player_turn % TWO +ONE == TWO ? Hit(mat_sub_player1, mat_guess_player2, row, col), Kill(subs_count_player1, mat_sub_player1, row, col) : ZERO; 
 		
-		// using the players turn we can know whom counter to change
-		// check
-		players_turn % TWO == ZERO ? Kill(ships_count_player1, mat, row, col);
-		players_turn % TWO != ZERO ? Kill(ships_count_player2, mat, row, col);		
-		
-		// update which player now needs to play
-		// check
-		players_turn++;
-		
-		// update if anyone won
-		is_someone_won = ships_count_player1 == ZERO || ships_count_player2 == ZERO;
+		counter++;
     }       
 }
 
